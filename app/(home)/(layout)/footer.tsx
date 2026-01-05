a3df897f2d71d1a20cbcee49df5a90e944875ac5
Francisco García <frank_garcia_dev@outlook.com>
2026-01-05 10:47:02 -0600

    Name update

--- app/(home)/(layout)/footer.tsx
+++ app/(home)/(layout)/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="grid place-items-center grid-cols-1 p-4 bg-secondary text-white">
-      <p>© 2024 Ecommerce by Pedro Francisco García Navarro</p>
+      <p>© 2024 Ecommerce by Francisco García</p>
     </footer>
   );
 }
